41a5e19da8be27011111f6e6ec9aec07f8b5e091
nutaalaibekov <nursultantaalaibekov1543@gmail.com>
2021-04-20 20:56:20 +0600

    Commit after file 12

--- src/kg/itacademy/NursultanTaalaibekov.java
+++ src/kg/itacademy/NursultanTaalaibekov.java
@@ -3,5 +3,6 @@ package kg.itacademy;
 public class NursultanTaalaibekov {
     public static void main(String[] args) {
         System.out.println("Hello, git");
+        System.out.println("Hello, some text ");
     }
 }
