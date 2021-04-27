dfd87af8da2680bf34e33a11774c7eb431101238
t.sabitov <t.sabitov@8bitgroup.com>
2021-04-27 20:26:46 +0600

    update

--- src/kg/itacademy/team2/Vladimir_Putin.java
+++ src/kg/itacademy/team2/Vladimir_Putin.java
@@ -2,6 +2,6 @@ package kg.itacademy.team2;
 
 public class Vladimir_Putin {
     public static void main(String[] args) {
-        System.out.println("Добрый вечер!");
+        System.out.println("Добрый вечер, я диспетчер!");
     }
 }
