1bb30dcaef95ec2a984c7b635e94f48bc378798e
EsenDzhumaev1 <esendzhumaev@gmail.com>
2021-04-27 20:39:47 +0600

    esen

--- src/kg/itacademy/team2/Vladimir_Putin.java
+++ src/kg/itacademy/team2/Vladimir_Putin.java
@@ -3,5 +3,6 @@ package kg.itacademy.team2;
 public class Vladimir_Putin {
     public static void main(String[] args) {
         System.out.println("Добрый вечер, я диспетчер!");
+        System.out.println("Владимир Путин - молодец! Политик, лидер и боец!"); //Esen
     }
 }
