8a406b5372d3868eee35f1f06877dcea343ad55c
nutaalaibekov <nursultantaalaibekov1543@gmail.com>
2021-04-27 15:45:34 +0600

    for lesson 13.03

--- src/kg/itacademy/Nurik.java
+++
@@ -1,8 +0,0 @@
-package kg.itacademy;
-
-public class Nurik {
-    public static void main(String[] args) {
-
-        System.out.println("SOme changes");
-    }
-}
--- src/kg/itacademy/NursultanTaalaibekov.java
+++
@@ -1,7 +0,0 @@
-package kg.itacademy;
-
-public class NursultanTaalaibekov {
-    public static void main(String[] args) {
-        System.out.println("Hello, git");
-    }
-}
