128366a16c97a676b3718bb25b9a0c3e11d8b2dc
nutaalaibekov <nursultantaalaibekov1543@gmail.com>
2021-04-29 19:48:41 +0600

    lesson 16

--- src/kg/itacademy/lesson16/Rectangle.java
+++ src/kg/itacademy/lesson16/Rectangle.java
@@ -3,4 +3,12 @@ package kg.itacademy.lesson16;
 public class Rectangle {
     int width;
     int height;
+
+    public void setWidth(int w) {
+        width = w;
+    }
+
+    public void setHeight(int h) {
+        height = h;
+    }
 }
--- src/kg/itacademy/lesson16/Task1.java
+++ src/kg/itacademy/lesson16/Task1.java
@@ -3,5 +3,8 @@ package kg.itacademy.lesson16;
 public class Task1 {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle();
+        rectangle.setHeight(6);
+        rectangle.setWidth(6);
+        System.out.println(rectangle.height * rectangle.width);
     }
 }
