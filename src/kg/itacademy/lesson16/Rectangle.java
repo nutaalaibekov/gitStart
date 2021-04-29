59b677d8a4003c488a4acb25eeada65542507fd1
nutaalaibekov <nursultantaalaibekov1543@gmail.com>
2021-04-29 19:27:36 +0600

    lesson 16

---
+++ src/kg/itacademy/lesson16/Rectangle.java
@@ -0,0 +1,6 @@
+package kg.itacademy.lesson16;
+
+public class Rectangle {
+    int width;
+    int height;
+}
---
+++ src/kg/itacademy/lesson16/Task1.java
@@ -0,0 +1,7 @@
+package kg.itacademy.lesson16;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+    }
+}
