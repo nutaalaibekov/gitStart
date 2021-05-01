bf3f9c8f3dde5a88ec2b76c2961f134d2d17525c
nutaalaibekov <nursultantaalaibekov1543@gmail.com>
2021-05-01 21:12:34 +0600

    lesson 17

--- src/kg/itacademy/lesson16/Task1.java
+++ src/kg/itacademy/lesson16/Task1.java
@@ -1,6 +1,7 @@
 package kg.itacademy.lesson16;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class Task1 {
     public static void main(String[] args) {
@@ -11,5 +12,29 @@ public class Task1 {
         System.out.println(black);
         System.out.println(yellow);
         System.out.println(hamleon);
+
+        int n = 5;
+        hamleon.setRadius(hamleon.getRadius() * n);
+        System.out.println(hamleon);
+        System.out.println(hamleon.getLength());
+        System.out.println(hamleon.getSquare());
+
+        Random random = new Random(100);
+        Circle[] arrCricle = new Circle[100];
+        int count = 0;
+        int radius = 0;
+        for (int i = 0; i < 100; i++) {
+            radius = random.nextInt();
+            if (radius % 3 == 0) {
+                arrCricle[count++] = new Circle(radius);
+            }
+        }
+
+        for (int i = 0; i <= count; i++) {
+            if (i % 3 == 0) {
+                System.out.println(arrCricle[i]);
+            }
+        }
+
     }
 }
