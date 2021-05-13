c09c0997b85e26c3f4109e5731e5a2822d5c009c
nutaalaibekov <nursultantaalaibekov1543@gmail.com>
2021-05-13 19:51:28 +0600

    lesson 22

--- src/kg/itacademy/lesson22/Counter.java
+++ src/kg/itacademy/lesson22/Counter.java
@@ -6,4 +6,12 @@ public class Counter {
         numOfCalls++;
         System.out.println(numOfCalls);
     }
+    public static int factorial() {
+        int factorial = 1;
+        for(int i = 1; i <= numOfCalls; i++) {
+            factorial = factorial * i;
+        }
+        System.out.println("factorial = " + factorial);
+        return factorial;
+    }
 }
--- src/kg/itacademy/lesson22/Main.java
+++ src/kg/itacademy/lesson22/Main.java
@@ -4,6 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Counter counter1 = new Counter();
         Counter counter2 = new Counter();
+
         Counter.increment();
+        Counter.increment();
+        Counter.increment();
+
+        Counter counter12 = new Counter();
+        Counter counter22 = new Counter();
+
+        Counter.increment();
+        Counter.increment();
+        Counter.increment();
+
+        Counter.factorial();
     }
 }
