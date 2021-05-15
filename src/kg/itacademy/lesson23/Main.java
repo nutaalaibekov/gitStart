81b63827a6540817d76b54d92cc09bafa5024f96
nutaalaibekov <nursultantaalaibekov1543@gmail.com>
2021-05-15 20:28:21 +0600

    lesson 23

---
+++ src/kg/itacademy/lesson23/Main.java
@@ -0,0 +1,15 @@
+package kg.itacademy.lesson23;
+
+public class Main {
+    public static void main(String[] args) {
+        final int[] arrFinal = new int[10];
+        for(int i = 0; i < arrFinal.length; i++) {
+            arrFinal[i] = i;
+            System.out.printf(i + "=" + arrFinal[i] + "    ");
+        }
+
+//        arrFinal = new int[10];
+
+
+    }
+}
