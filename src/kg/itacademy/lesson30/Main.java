fcd0994c55bd8ba3fc82698fb4386d510e7d9d89
nutaalaibekov <nursultantaalaibekov1543@gmail.com>
2021-06-08 20:38:10 +0600

    lesson 30

--- src/kg/itacademy/lesson30/Main.java
+++ src/kg/itacademy/lesson30/Main.java
@@ -1,4 +1,20 @@
 package kg.itacademy.lesson30;
 
 public class Main {
+    public static void main(String[] args) {
+        Cat[] cats = new Cat[5];
+        cats[0] = new Cat("Cat1");
+        cats[1] = new Cat("Cat2");
+        cats[2] = new Cat("Cat3");
+        cats[3] = null;
+        cats[4] = new Cat("Cat5");
+
+        for(Cat cat : cats) {
+            try {
+                System.out.println(cat.getName());
+            } catch (NullPointerException npe) {
+                System.out.println("NullPointerException ЗАБРАЛИ КОШКУ");
+            }
+        }
+    }
 }
