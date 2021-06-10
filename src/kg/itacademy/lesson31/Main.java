92f0f8c92714e5919c7ca6e6512c70b596cb57c1
nutaalaibekov <nursultantaalaibekov1543@gmail.com>
2021-06-10 20:50:39 +0600

    lesson 31

--- src/kg/itacademy/lesson31/Main.java
+++ src/kg/itacademy/lesson31/Main.java
@@ -5,19 +5,18 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> squares = new ArrayList<>(100);
-
-        int countNotOdd = 0;
-        for(int i = 1; i <= 100; i++) {
-
-            if (i % 2 != 0) {
-                squares.add(countNotOdd, i);
-                countNotOdd++;
+        ArrayList<Integer> indexes = new ArrayList<>(100);
+        for(int i = 1; i <= 10000; i++) {
+            if(i * i <= 10000) {
+                squares.add(i * i);
+                if (squares.get(squares.size() - 1) % 2 == 0 || squares.get(squares.size() - 1) % 3 == 0)
+                     indexes.add(squares.size() - 1);
             } else {
-                squares.add(i);
+                break;
             }
         }
 
         System.out.println(squares);
-
+        System.out.println(indexes);
     }
 }
